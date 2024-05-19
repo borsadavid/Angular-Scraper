7836672022e7bbeba28b817c77702480e2061bdf
ccc909 <57506761+ccc909@users.noreply.github.com>
2024-05-19 15:37:26 +0300

    fix sending string instead of any

--- src/app/websocket-service/websocket.service.ts
+++ src/app/websocket-service/websocket.service.ts
@@ -1,6 +1,7 @@
 // Import the necessary modules and dependencies
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,24 @@ export class WebsocketService {
     return this.socket$;
   }
 
-  sendMessage(message: string): void {
+  sendMessage(message: any): void {
     this.socket$.next(message);
+  }
+
+  receiveOneMessage(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.socket$.pipe(first()).subscribe({
+        next: (message) => {
+          resolve(message);
+        },
+        error: (err) => {
+          reject(err);
+        },
+        complete: () => {
+          // WebSocket completed
+        }
+      });
+    });
   }
 
   close(): void {
